37b559315831760e6ad59cc652ee4782c5270514
Jade <dillenkofer.jonas@outlook.de>
2024-10-27 14:35:01 +0100

    fix mobile blogpost view

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,10 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import tunnel from 'astro-tunnel';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind(), icon()],
+    integrations: [mdx(), sitemap(), tailwind(), icon(), tunnel()],
 });
