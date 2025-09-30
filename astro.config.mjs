0cebea8413524033132b30b32f168ef8f04bf8dc
Jade <jadefw@proton.me>
2025-09-30 14:12:14 +0200

    added epk

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,4 @@ import react from '@astrojs/react';
 export default defineConfig({
     site: 'https://example.com',
     integrations: [mdx(), sitemap(), tailwind(), icon(), tunnel(), react()],
-    redirects: {
-        '/pdf': 'https://www.papermark.com/view/cmfwrh68c0003ld04v5g2p54k'
-    }
 });
